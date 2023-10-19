25be0fd160531b163ea2ccffba90c459f9593999
Mofassel100 <smmofasselhosain.bba.ac.22@gmail.com>
2023-10-19 12:26:41 +0600

    dd

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -6,11 +6,10 @@ import { Button, Col, Row, message } from "antd";
 import Image from "next/image";
 
 import Form from "../Forms/Form";
-import { yupResolver } from "@hookform/resolvers/yup";
 import FormInput from "../Forms/FormInput";
 import { SubmitHandler } from "react-hook-form";
 import {  useRouter } from "next/navigation";
-import { loginSchema } from "@/lib/schemas/login";
+
 
 
 
@@ -69,7 +68,7 @@ const {role }= getUserInfo()
           Plase login your account
         </h1>
         <div>
-          <Form submitHandler={onSubmit} resolver={yupResolver(loginSchema)}>
+          <Form submitHandler={onSubmit}>
             <div>
               <FormInput
                 name="email"
